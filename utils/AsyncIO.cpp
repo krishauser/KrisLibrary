d908916ec82000445031b448d643108147ae3228
krishauser <hauser.kris@gmail.com>
2014-02-21 18:58:43 -0500

    Added asynchronous writing functionality, server sockets

--- utils/AsyncIO.cpp
+++ utils/AsyncIO.cpp
@@ -1,11 +1,12 @@
 #include "AsyncIO.h"
+#include "socketutils.h"
 #include <iostream>
 
 AsyncReaderQueue::AsyncReaderQueue(size_t _queueMax)
   :queueMax(_queueMax),msgCount(0)
 {}
 
-void AsyncReaderQueue::Enqueue(const string& msg)
+void AsyncReaderQueue::OnRead(const string& msg)
 {
   while(msgQueue.size()>=queueMax)
     msgQueue.pop_front();
@@ -28,6 +29,38 @@ vector<string> AsyncReaderQueue::NewMessages()
   return res;
 }
 
+AsyncWriterQueue::AsyncWriterQueue(size_t _queueMax)
+  :queueMax(_queueMax),msgCount(0)
+{}
+
+
+string AsyncWriterQueue::OnWrite()
+{
+  if(msgQueue.empty()) return "";
+  string res = msgQueue.front();
+  while(msgQueue.size()>=queueMax)
+    msgQueue.pop_front();
+  msgQueue.pop_front();
+  msgCount += 1;
+  return res;
+}
+
+void AsyncWriterQueue::Reset()
+{
+  msgQueue.clear();
+  msgCount = 0;
+}
+
+void AsyncWriterQueue::SendMessage(const string& msg)
+{
+  msgQueue.push_back(msg);
+}
+
+
+AsyncPipeQueue::AsyncPipeQueue(size_t _recvQueueMax,size_t _sendQueueMax)
+  :reader(_recvQueueMax),writer(_sendQueueMax)
+{}
+
 
 AsyncReaderThread::AsyncReaderThread(double _timeout)
   :AsyncReaderQueue(1),initialized(false),timeout(_timeout),lastReadTime(-1)
@@ -37,6 +70,7 @@ AsyncReaderThread::AsyncReaderThread(double _timeout)
 
 AsyncReaderThread::~AsyncReaderThread()
 {
+  Stop();
 }
 
 void AsyncReaderThread::Reset()
@@ -50,14 +84,15 @@ void* read_worker_thread_func(void * ptr)
 {
   AsyncReaderThread* data = reinterpret_cast<AsyncReaderThread*>(ptr);
   while(data->timer.ElapsedTime() < data->lastReadTime + data->timeout) {
-    const char* res = data->Callback();
+    const char* res = data->transport->DoRead();
     if(!res) {
       fprintf(stderr,"AsyncReaderThread: abnormal termination\n");
       return NULL;
     }
     
     pthread_mutex_lock(&data->mutex);
-    data->Enqueue(res);
+    data->OnRead(res);
+    data->lastReadTime = data->timer.ElapsedTime();
     pthread_mutex_unlock(&data->mutex);
   }
   return NULL;
@@ -67,6 +102,8 @@ void* read_worker_thread_func(void * ptr)
 bool AsyncReaderThread::Start()
 {
   if(!initialized) {
+    if(!transport) return false;
+    if(!transport->Start()) return false;
     lastReadTime = 0;
     pthread_create(&thread,NULL,read_worker_thread_func,this);
     initialized = true;
@@ -79,17 +116,115 @@ void AsyncReaderThread::Stop()
   if(initialized) {
     timeout = 0;
     pthread_join(thread,NULL);
+    transport->Stop();
     initialized = false;
   }
 }
 
 
-SocketReadWorker::SocketReadWorker(const char* _addr,double _timeout)
-  :AsyncReaderThread(_timeout),addr(_addr)
+
+
+AsyncPipeThread::AsyncPipeThread(double _timeout)
+  :AsyncPipeQueue(1),initialized(false),timeout(_timeout),lastReadTime(-1)
+{
+  mutex = PTHREAD_MUTEX_INITIALIZER;
+}
+
+AsyncPipeThread::~AsyncPipeThread()
+{
+  Stop();
+}
+
+void AsyncPipeThread::Reset()
+{
+  Stop();
+  AsyncPipeQueue::Reset();
+  Start();
+}
+
+void* pipe_read_worker_thread_func(void * ptr)
+{
+  AsyncPipeThread* data = reinterpret_cast<AsyncPipeThread*>(ptr);
+  while(data->timer.ElapsedTime() < data->lastReadTime + data->timeout) {
+    if(!data->transport->ReadReady()) {
+      pthread_yield();
+    }
+    else {
+      const char* res = data->transport->DoRead();
+      if(!res) {
+	fprintf(stderr,"AsyncReaderThread: abnormal termination\n");
+	return NULL;
+      }    
+      pthread_mutex_lock(&data->mutex);
+      data->OnRead(res);
+      data->lastReadTime = data->timer.ElapsedTime();
+      pthread_mutex_unlock(&data->mutex);
+    }
+  }
+  return NULL;
+}
+
+void* pipe_write_worker_thread_func(void * ptr)
+{
+  AsyncPipeThread* data = reinterpret_cast<AsyncPipeThread*>(ptr);
+  while(data->timer.ElapsedTime() < data->lastWriteTime + data->timeout) {
+    //do the writing
+    if(!data->transport->WriteReady()) {
+      pthread_yield();
+    }
+    else {
+      string send;
+      pthread_mutex_lock(&data->mutex);
+      if(data->WriteAvailable()) {
+	 send = data->OnWrite();
+	 data->lastWriteTime = data->timer.ElapsedTime();
+      }
+      pthread_mutex_unlock(&data->mutex);
+      if(!send.empty()) {
+	if(!data->transport->DoWrite(send.c_str())) {
+	  fprintf(stderr,"AsyncPipeThread: abnormal termination\n");
+	  return NULL;
+	}
+      }
+      else
+	pthread_yield();
+    }    
+  }
+  return NULL;
+}
+
+
+bool AsyncPipeThread::Start()
+{
+  if(!transport) return false;
+  if(!initialized) {
+    if(!transport->Start()) return false;
+    lastReadTime = lastWriteTime = 0;
+    pthread_create(&readThread,NULL,pipe_read_worker_thread_func,this);
+    pthread_create(&readThread,NULL,pipe_write_worker_thread_func,this);
+    initialized = true;
+  }
+  return true;
+}
+
+void AsyncPipeThread::Stop()
+{
+  if(initialized) {
+    timeout = 0;
+    pthread_join(readThread,NULL);
+    pthread_join(writeThread,NULL);
+    transport->Stop();
+    initialized = false;
+  }
+}
+
+
+SocketClientTransport::SocketClientTransport(const char* _addr)
+  :addr(_addr)
 {
 }
 
-const char* SocketReadWorker::Callback()
+const char* SocketClientTransport::DoRead()
 {
   if(!socket.ReadString(buf,4096)) {
     cout<<"SocketReadWorker: Error reading string..."<<endl;
@@ -98,26 +233,115 @@ const char* SocketReadWorker::Callback()
   return buf;
 }
 
-bool SocketReadWorker::Start()
+bool SocketClientTransport::Start()
 {
-  cout<<"SocketReadWorker: Creating subscriber socket..."<<endl;
+  cout<<"SocketClientTransport: Creating socket on "<<addr<<"..."<<endl;
   bool opened = false;
   while(!opened) {
-    if(!socket.Open(addr.c_str(),FILEREAD)) {
-      fprintf(stderr,"SocketReadWorker: Unable to open address... waiting");
+    if(!socket.Open(addr.c_str(),FILECLIENT)) {
+      fprintf(stderr,"SocketTransport: Unable to open address... waiting\n");
       sleep(1);
     }
     else
       opened=true;
   }
   sleep(1);
-  return AsyncReaderThread::Start();
+  return true;
 }
 
-void SocketReadWorker::Stop()
+bool SocketClientTransport::Stop()
 {
-  AsyncReaderThread::Stop();
-  cout<<"SocketReadWorker: Destroying socket:"<<endl;
+  cout<<"SocketTransport: Destroying socket:"<<endl;
   socket.Close();
-  cout<<"SocketReadWorker: Done destroying socket:"<<endl;
+  cout<<"SocketTransport: Done destroying socket:"<<endl;
+  return true;
+}
+
+bool SocketClientTransport::DoWrite(const char* str)
+{
+  return socket.WriteString(str);
+}
+
+
+SocketServerTransport::SocketServerTransport(const char* _addr,int _maxclients)
+  :addr(_addr),serversocket(-1),maxclients(_maxclients),currentclient(-1)
+{
+}
+
+bool SocketServerTransport::Start()
+{
+  serversocket = Bind(addr.c_str(),true);
+  if(serversocket < 0) {
+    fprintf(stderr,"Unable to bind to address %s\n",addr.c_str());
+    return false;
+  }
+  listen(serversocket,maxclients);
+  return true;
+}
+
+bool SocketServerTransport::Stop()
+{
+  for(size_t i=0;i<clientsockets.size();i++)
+    clientsockets[i] = NULL;
+  clientsockets.resize(0);
+  close(serversocket);
+  return true;
+}
+
+bool SocketServerTransport::ReadReady()
+{
+  if((int)clientsockets.size() < maxclients) {
+    int clientsock = Accept(serversocket);
+    if(clientsock >= 0) {
+      printf("Accepted new client on %s\n",addr.c_str());
+      clientsockets.push_back(new File);
+      clientsockets.back()->OpenTCPSocket(clientsock);
+    }
+  }
+  return !clientsockets.empty();
+}
+
+bool SocketServerTransport::WriteReady()
+{
+  return !clientsockets.empty();
+}
+
+const char* SocketServerTransport::DoRead()
+{
+  if(clientsockets.empty()) return NULL;
+  currentclient = (currentclient+1) % clientsockets.size();
+  while(!clientsockets.empty()) {
+    //loop through the sockets
+    if(clientsockets[currentclient]->ReadString(buf,4096))
+      return buf;
+    //close the client
+    printf("SocketServer: Lost client %d\n",currentclient);
+    clientsockets[currentclient] = NULL;
+    clientsockets[currentclient] = clientsockets.back();
+    clientsockets.resize(clientsockets.size()-1);
+    if(clientsockets.empty()) break;
+    currentclient = currentclient % clientsockets.size();
+  }
+  //should we be tolerant of failed clients?
+  buf[0] = 0;
+  return buf;
+  return NULL;
+}
+
+bool SocketServerTransport::DoWrite(const char* str)
+{
+  for(size_t i=0;i<clientsockets.size();i++) {
+    if(!clientsockets[i]->WriteString(str)) {
+      printf("SocketServer: Lost client %d\n",i);
+      //close the client
+      clientsockets[i] = NULL;
+      clientsockets[i] = clientsockets.back();
+      clientsockets.resize(clientsockets.size()-1);
+      i--;
+    }
+  }
+  //should we be tolerant of failed clients?
+  return true;
+  if(clientsockets.empty()) return false;
+  return true;
 }
--- utils/AsyncIO.h
+++ utils/AsyncIO.h
@@ -8,6 +8,7 @@
 #include <Timer.h>
 #include <errors.h>
 #include <math/math.h>
+#include "SmartPointer.h"
 #ifndef WIN32
 #include <pthread.h>
 #endif //WIN32
@@ -15,12 +16,16 @@ using namespace std;
 
 /** @brief Asynchronous reader base class.
  * 
- * Message writer is implemented by subclass. It may store an internal queue of messages.
+ * Message storage is implemented by subclass. It may store an internal queue of messages.
+ * @sa AsyncReaderQueue
  *
- * Message reader will call NewMessages() or NewestMessage() to get the latest message. This will also flush the queue.
+ * Usage is to call NewMessages() or NewestMessage() to get the latest message. This will also
+ * flush the queue.
+ * 
  * To do a non-blocking check for new messages without flushing the queue, call NewMessageCount().
  *
- * Event-driven readers can be implemented by overloading the Work() class.
+ * Event-driven readers can be implemented by overloading the Work() class.  The user must
+ * then periodically call Work() to allow the reader to process messages.
  */
 class AsyncReader
 {
@@ -36,14 +41,42 @@ class AsyncReader
   virtual string NewestMessage() { if(NewMessageCount()>0) return NewMessages().back(); return LastMessage(); }
 };
 
-///stores the last messages and the newest queueMax messages
+/** @brief Asynchronous writer base class.
+ * 
+ * Message queuing can be implemented in the subclass.
+ * @sa AsyncWriterQueue.
+ *
+ * The usage is to call SendMessage().  The writer will then somehow
+ * send it to a receiver (as implemented by the subclass).
+ *
+ * Event-driven writers can be implemented by overloading the Work() class.  The user must
+ * then periodically call Work() to allow the reader to process messages.
+ */
+class AsyncWriter
+{
+ public:
+  AsyncWriter() {}
+  virtual ~AsyncWriter() {}
+  virtual void Reset() { }
+  virtual void Work() {}
+  virtual void SendMessage(const string& msg) {}
+  virtual int SentMessageCount() { return 0; }
+  virtual int DeliveredMessageCount() { return 0; }
+};
+
+
+/** @brief Asynchronous reader with queue.
+ *
+ * stores the last message and the newest queueMax messages.
+ */
 class AsyncReaderQueue : public AsyncReader
 {
  public:
   ///This will keep only the newest queueMax messages
   AsyncReaderQueue(size_t queueMax=1000);
-  ///Called by writer subclass to add a message onto the queue
-  void Enqueue(const string& msg);
+  ///Called by subclass to add a message onto the queue
+  void OnRead(const string& msg);
+
   ///Resets the queue and history
   virtual void Reset();
   virtual int MessageCount() { return (int)msgCount; }
@@ -55,6 +88,127 @@ class AsyncReaderQueue : public AsyncReader
   size_t msgCount;
   string msgLast;
   deque<string> msgQueue;
+};
+
+/** @brief Asynchronous writer with queue.
+ *
+ * Sends the latest queueMax messages.
+ */
+class AsyncWriterQueue : public AsyncWriter
+{
+ public:
+  ///This will keep only the newest recvQueueMax messages sent, and will only
+  ///allow an overflow of sendQueueMax messages.  Typical usage may be 1 and 1
+  ///(only keep and send newest messages).
+  AsyncWriterQueue(size_t queueMax=1000);
+
+  ///Called by subclass to see whether there's a message to send
+  bool WriteAvailable() const { return !msgQueue.empty(); }
+  ///Called by subclass to get the next message to deliver to the destination
+  string OnWrite();
+
+  virtual void Reset();
+  virtual void SendMessage(const string& msg);
+  virtual int SentMessageCount() { return msgCount+msgQueue.size(); }
+  virtual int DeliveredMessageCount() { return msgCount; }
+
+  size_t queueMax;
+  size_t msgCount;
+  deque<string> msgQueue;
+};
+
+/** @brief Asynchronous reader/writer with queues.
+ */
+class AsyncPipeQueue
+{
+ public:
+  AsyncPipeQueue(size_t recvQueueSize=1000,size_t writeQueueSize=1000);
+  virtual void Reset() { reader.Reset(); writer.Reset(); }
+  virtual void Work() { reader.Work(); writer.Work(); }
+
+  ///Interfaces that subclasses should use in Work()
+  ///Called by subclass to add a message onto the queue
+  void OnRead(const string& msg) { return reader.OnRead(msg); }
+  ///Called by subclass to see whether there's a message to send
+  bool WriteAvailable() const { return writer.WriteAvailable(); }
+  ///Called by subclass to get the next message to send to the destination
+  string OnWrite() { return writer.OnWrite(); }
+
+  ///Receive functions
+  int MessageCount() { return reader.MessageCount(); }
+  int NewMessageCount() { return reader.NewMessageCount(); }
+  string LastMessage() { return reader.LastMessage(); }
+  vector<string> NewMessages() { return reader.NewMessages(); }
+  string NewestMessage() { return reader.NewestMessage(); }
+
+  ///Send functions
+  void SendMessage(const string& msg) { writer.SendMessage(msg); }
+  int SentMessageCount() { return writer.SentMessageCount(); }
+  int DeliveredMessageCount() { return writer.DeliveredMessageCount(); }
+
+
+  AsyncReaderQueue reader;
+  AsyncWriterQueue writer;
+};
+
+class AsyncTransport
+{
+ public:
+  AsyncTransport() {}
+  virtual ~AsyncTransport() {}
+  ///Subclasses -- can the object read?
+  virtual bool Start() { return true; }
+  virtual bool Stop() { return true; }
+  ///Subclasses -- can the object read?
+  virtual bool ReadReady() { return true; }
+  ///Subclasses -- can the object write?
+  virtual bool WriteReady() { return true; }
+  ///Subclasses -- in thread, do some I/O and processing (which may block), return the payload message.
+  ///If NULL is returned, there is some error and the thread quits
+  virtual const char* DoRead() { return NULL; }
+  ///Subclasses -- in thread, do some I/O and processing (which may block)
+  ///If false is returned, there is some error and the thread quits
+  virtual bool DoWrite(const char* msg) { return false; }
+};
+
+/** @brief A transport protocol that connects as a client to the given address. */
+class SocketClientTransport : public AsyncTransport
+{
+ public:
+  SocketClientTransport(const char* addr);
+  virtual bool Start();
+  virtual bool Stop();
+  ///Reads a string (4 byte length + data). Note: blocking
+  virtual const char* DoRead();
+  ///Writes a string (4 byte length + data). Note: blocking.
+  virtual bool DoWrite (const char* str);
+
+  string addr;
+  File socket;
+  char buf[4096];
+};
+
+/** @brief A transport protocol that hosts a client and sends messages to
+ * the clients after they connect . */
+class SocketServerTransport : public AsyncTransport
+{
+ public:
+  SocketServerTransport(const char* addr,int maxclients=1);
+  virtual bool Start();
+  virtual bool Stop();
+  virtual bool ReadReady();
+  virtual bool WriteReady();
+  ///Reads a string (4 byte length + data) from all clients. Note: blocking
+  virtual const char* DoRead();
+  ///Writes a string (4 byte length + data) to all clients. Note: blocking.
+  virtual bool DoWrite (const char* str);
+
+  string addr;
+  int serversocket;
+  int maxclients;
+  vector<SmartPointer<File> > clientsockets;
+  int currentclient;
+  char buf[4096];
 };
 
 
@@ -75,9 +229,7 @@ class AsyncReaderThread : public AsyncReaderQueue
   virtual bool Start();
   virtual void Stop();
 
-  ///Subclasses -- in thread, do some I/O and processing (which may block), return the payload message.
-  ///If NULL is returned, there is some error and the thread quits
-  virtual const char* Callback() { return NULL; }
+  SmartPointer<AsyncTransport> transport;
 
   bool initialized;
   pthread_t thread;
@@ -85,24 +237,60 @@ class AsyncReaderThread : public AsyncReaderQueue
 
   Timer timer;
   pthread_mutex_t mutex;
-  string payload;
-  bool newMessage;
   double lastReadTime;
 };
 
+///An asynchronous reader that uses multithreading.
+///Subclass will define what that particular process is (usually blocking I/O)
+///by overloading the Callback() function.
+///If no read/write has occurred for 'timeout' seconds, then the thread will quit
+class AsyncPipeThread : public AsyncPipeQueue
+{
+ public:
+  AsyncPipeThread(double timeout=Math::Inf);
+  ~AsyncPipeThread();
+  virtual void Reset();
+  ///Subclasses: override these to implement custom starting and stopping routines
+  virtual bool Start();
+  virtual void Stop();
 
-///Lets you read the last message sent to a socket.
+  SmartPointer<AsyncTransport> transport;
+
+  bool initialized;
+  pthread_t readThread,writeThread;
+  double timeout;
+
+  Timer timer;
+  pthread_mutex_t mutex;
+  double lastReadTime;
+  double lastWriteTime;
+};
+
+
 class SocketReadWorker : public AsyncReaderThread
 {
  public:
-  SocketReadWorker(const char* addr,double timeout=Math::Inf);
-  virtual bool Start();
-  virtual void Stop();
-  virtual const char* Callback();
+  SocketReadWorker(const char* addr,bool asServer=false,double timeout=Math::Inf)
+    :AsyncReaderThread(timeout)
+  {
+    if(asServer)
+      transport = new SocketServerTransport(addr);
+    else
+      transport = new SocketClientTransport(addr);
+  }
+};
 
-  string addr;
-  File socket;
-  char buf[4096];
+class SocketPipeWorker : public AsyncPipeThread
+{
+ public:
+  SocketPipeWorker(const char* addr,bool asServer=false,double timeout=Math::Inf)
+    :AsyncPipeThread(timeout)
+  {
+    if(asServer)
+      transport = new SocketServerTransport(addr);
+    else
+      transport = new SocketClientTransport(addr);
+  }
 };
 
 #endif //WIN32
