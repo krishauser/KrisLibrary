c81b69c4815d5ecd84317146e007242cd6ecedee
krishauser <hauser.kris@gmail.com>
2014-04-29 23:00:25 -0400

    Added threading code for windows

---
+++ utils/threadutils.h
@@ -0,0 +1,55 @@
+#ifndef THREAD_UTILS_H
+#define THREAD_UTILS_H
+
+#ifdef WIN32
+#define USE_BOOST_THREADS 1
+#define USE_PTHREADS 0
+#else
+#define USE_BOOST_THREADS 1
+#define USE_PTHREADS 0
+#endif
+
+#if USE_BOOST_THREADS
+#include <boost/thread.hpp>
+typedef boost::thread Thread;
+typedef boost::mutex Mutex;
+typedef boost::mutex::scoped_lock ScopedLock;
+inline Thread ThreadStart(void* (*fn)(void*),void* data=NULL) { return boost::thread(fn,data); }
+inline void ThreadJoin(Thread& thread) { thread.join(); }
+inline void ThreadYield() { boost::this_thread::yield(); }
+
+#endif //USE_BOOST_THREADS
+
+#if USE_PTHREADS
+#include <pthread.h>
+typedef pthread_t Thread;
+inline Thread ThreadStart(void* (*fn)(void*),void* data=NULL) {
+	pthread_t thread;
+	pthread_create(&thread,NULL,fn,data);
+	return thread;
+}
+inline void ThreadJoin(Thread& thread) { pthread_join(thread,NULL); }
+inline void ThreadYield() { pthread_yield(); }
+struct Mutex
+{
+	Mutex() { mutex = PTHREAD_MUTEX_INITIALIZER; }
+	pthread_mutex_t mutex;
+};
+
+struct ScopedLock
+{
+	ScopedLock(Mutex& _mutex) { mutex=&_mutex; pthread_mutex_lock(mutex.mutex);  }
+	~ScopedLock() { pthread_mutex_unlock(&mutex.mutex); }
+	Mutex& mutex;
+}
+
+#endif //USE_PTHREADS
+
+#ifdef WIN32
+inline void ThreadSleep(double duration) { Sleep(int(duration*1000)); }
+#else
+#include <unistd.h>
+inline void ThreadSleep(double duration) { usleep(int(duration*1000000)); }
+#endif
+
+#endif //THREAD_UTILS_H
