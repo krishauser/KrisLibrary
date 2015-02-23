9aec4f1389878cc677a9aa8ef53175640311f9c5
krishauser <hauser.kris@gmail.com>
2015-02-22 21:13:43 -0500

    ToBase64 / FromBase64 now more memory efficient for large encodings

--- utils/stringutils.cpp
+++ utils/stringutils.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <assert.h>
 
 char CloseBracket(char c)
 {
@@ -549,13 +550,15 @@ static inline bool is_base64(unsigned char c) {
   return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
-std::string base64_encode(const char* bytes_to_encode, unsigned int in_len) {
-  std::string ret;
+std::string base64_encode(const char* bytes_to_encode, unsigned int in_len, std::string& ret) {
+  unsigned int out_len = (in_len+2)/3*4;
+  ret.resize(out_len);
   int i = 0;
   int j = 0;
   unsigned char char_array_3[3];
   unsigned char char_array_4[4];
 
+  unsigned int k=0;
   while (in_len--) {
     char_array_3[i++] = *(bytes_to_encode++);
     if (i == 3) {
@@ -564,8 +567,8 @@ std::string base64_encode(const char* bytes_to_encode, unsigned int in_len) {
       char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
       char_array_4[3] = char_array_3[2] & 0x3f;
 
-      for(i = 0; (i <4) ; i++)
-        ret += base64_chars[char_array_4[i]];
+      for(i = 0; (i <4) ; i++,k++)
+        ret[k] = base64_chars[char_array_4[i]];
       i = 0;
     }
   }
@@ -580,25 +583,26 @@ std::string base64_encode(const char* bytes_to_encode, unsigned int in_len) {
     char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
     char_array_4[3] = char_array_3[2] & 0x3f;
 
-    for (j = 0; (j < i + 1); j++)
-      ret += base64_chars[char_array_4[j]];
+    for (j = 0; (j < i + 1); j++, k++)
+      ret[k] = base64_chars[char_array_4[j]];
 
-    while((i++ < 3))
-      ret += '=';
-
+    while((i++ < 3)) {
+      ret[k] = '=';
+	  k++;
+	}
   }
-
-  return ret;
-
+  assert(k == out_len);
 }
 
-std::string base64_decode(const char* encoded_string, unsigned int in_len) {
+void base64_decode(const char* encoded_string, unsigned int in_len,std::string& ret) {
   int i = 0;
   int j = 0;
   int in_ = 0;
   unsigned char char_array_4[4], char_array_3[3];
-  std::string ret;
+  unsigned int out_len = (in_len+3)/4*3;
+  ret.resize(out_len);
 
+  unsigned int k=0;
   while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
     char_array_4[i++] = encoded_string[in_]; in_++;
     if (i ==4) {
@@ -609,8 +613,8 @@ std::string base64_decode(const char* encoded_string, unsigned int in_len) {
       char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
       char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
+      for (i = 0; (i < 3); i++,k++)
+        ret[k] = char_array_3[i];
       i = 0;
     }
   }
@@ -626,30 +630,59 @@ std::string base64_decode(const char* encoded_string, unsigned int in_len) {
     char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
     char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+    for (j = 0; (j < i - 1); j++,k++) ret[k] = char_array_3[j];
   }
-
-  return ret;
+  assert(k == out_len);
 }
+
+void ToBase64(const std::string& in,std::string& out)
+{
+  base64_encode(in.c_str(),in.length(),out);
+}
+
+void ToBase64(const char* in,int length,std::string& out)
+{
+  base64_encode(in,length,out);
+}
+
+void FromBase64(const std::string& in,std::string& out)
+{
+  base64_decode(in.c_str(),in.length(),out);
+}
+
+void FromBase64(const char* in,std::string& out)
+{
+  base64_decode(in,strlen(in),out);
+}
+
+
 
 std::string ToBase64(const std::string& in)
 {
-  return base64_encode(in.c_str(),in.length());
+	std::string res;
+	ToBase64(in,res);
+	return res;
 }
 
 std::string ToBase64(const char* in,int length)
 {
-  return base64_encode(in,length);
+	std::string res;
+	ToBase64(in,length,res);
+	return res;
 }
 
 std::string FromBase64(const std::string& in)
 {
-  return base64_decode(in.c_str(),in.length());
+	std::string res;
+	FromBase64(in,res);
+	return res;
 }
 
 std::string FromBase64(const char* in)
 {
-  return base64_decode(in,strlen(in));
+	std::string res;
+	FromBase64(in,res);
+	return res;
 }
 
 
