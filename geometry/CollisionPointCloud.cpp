dff8f755c5fe9b548a9a91fe5dce2ebde1f5183f
krishauser <hauser.kris@gmail.com>
2015-03-15 01:58:44 -0400

    Added point cloud collision accelerators

--- geometry/CollisionPointCloud.cpp
+++ geometry/CollisionPointCloud.cpp
@@ -1,14 +1,16 @@
 #include "CollisionPointCloud.h"
+#include <Timer.h>
 
 namespace Geometry {
 
 CollisionPointCloud::CollisionPointCloud()
+  :gridResolution(0),grid(3)
 {
   currentTransform.setIdentity();
 }
 
 CollisionPointCloud::CollisionPointCloud(const Meshing::PointCloud3D& _pc)
-  :Meshing::PointCloud3D(_pc)
+  :Meshing::PointCloud3D(_pc),gridResolution(0),grid(3)
 {
   currentTransform.setIdentity();
   InitCollisions();
@@ -16,14 +18,55 @@ CollisionPointCloud::CollisionPointCloud(const Meshing::PointCloud3D& _pc)
 
 void CollisionPointCloud::InitCollisions()
 {
+  Timer timer;
   bblocal.minimize();
   for(size_t i=0;i<points.size();i++)
     bblocal.expand(points[i]);
+  //set up the grid
+  Real res = gridResolution;
+  if(gridResolution <= 0) {
+    Vector3 dims = bblocal.bmax-bblocal.bmin;
+    Real maxdim = Max(dims.x,dims.y,dims.z);
+    Real mindim = Min(dims.x,dims.y,dims.z);
+    //default grid size: assume points are evenly distributed on a 2D manifold
+    //in space, try to get 50 points per grid cell
+    Real vol = dims.x*dims.y*dims.z;
+    //h^2 * n = vol
+    int ptsPerCell = 50;
+    Real h = Pow(vol / points.size() * ptsPerCell, 1.0/2.0);
+    if(h > mindim) { 
+      //TODO: handle relatively flat point clouds
+    }
+    res = h;
+  }
+  grid.h.set(res);
+  for(size_t i=0;i<points.size();i++) {
+    Vector p(3,points[i]);
+    GridSubdivision::Index ind;
+    grid.PointToIndex(p,ind);
+    grid.Insert(ind,&points[i]);
+  }
+  printf("CollisionPointCloud::InitCollisions: %d points, res %g, time %gs\n",points.size(),res,timer.ElapsedTime());
+  //print stats
+  int nmax = 0;
+  for(GridSubdivision::HashTable::const_iterator i=grid.buckets.begin();i!=grid.buckets.end();i++)
+    nmax = Max(nmax,(int)i->second.size());
+  printf("  %d nonempty buckets, max size %d, avg %g\n",grid.buckets.size(),nmax,Real(points.size())/grid.buckets.size());
 }
 
 void GetBB(const CollisionPointCloud& pc,Box3D& b)
 {
   b.setTransformed(pc.bblocal,pc.currentTransform);
+}
+
+static Real gWithinDistanceTestThreshold = 0;
+static GeometricPrimitive3D* gWithinDistanceTestObject = NULL;
+bool withinDistanceTest(void* obj)
+{
+  Point3D* p = reinterpret_cast<Point3D*>(obj);
+  if(gWithinDistanceTestObject->Distance(*p) <= gWithinDistanceTestThreshold)
+    return false;
+  return true;
 }
 
 bool WithinDistance(const CollisionPointCloud& pc,const GeometricPrimitive3D& g,Real tol)
@@ -33,19 +76,72 @@ bool WithinDistance(const CollisionPointCloud& pc,const GeometricPrimitive3D& g,
   //quick reject test
   if(g.Distance(bb) > tol) return false;
 
-  //test all points, linearly
-  for(size_t i=0;i<pc.points.size();i++)
-    if(g.Distance(pc.points[i]) <= tol) return true;
-  return false;
+  GeometricPrimitive3D glocal = g;
+  RigidTransform Tinv;
+  Tinv.setInverse(pc.currentTransform);
+  glocal.Transform(Tinv);
+
+  AABB3D gbb = glocal.GetAABB();  
+  gbb.setIntersection(pc.bblocal);
+  GridSubdivision::Index imin,imax;
+  pc.grid.PointToIndex(Vector(3,gbb.bmin),imin);
+  pc.grid.PointToIndex(Vector(3,gbb.bmax),imax);
+  int numCells = (imax[0]-imin[0]+1)*(imax[1]-imin[1]+1)*(imax[2]-imin[2]+1);
+  if(numCells > (int)pc.points.size()) {
+    //test all points, linearly
+    for(size_t i=0;i<pc.points.size();i++)
+      if(glocal.Distance(pc.points[i]) <= tol) return true;
+    return false;
+  }
+  else {
+    gWithinDistanceTestThreshold = tol;
+    gWithinDistanceTestObject = &glocal;
+    bool collisionFree = pc.grid.IndexQuery(imin,imax,withinDistanceTest);
+    return !collisionFree;
+  }
+}
+
+static Real gDistanceTestValue = 0;
+static GeometricPrimitive3D* gDistanceTestObject = NULL;
+bool distanceTest(void* obj)
+{
+  Point3D* p = reinterpret_cast<Point3D*>(obj);
+  gDistanceTestValue = Min(gDistanceTestValue,gDistanceTestObject->Distance(*p));
+  return true;
 }
 
 Real Distance(const CollisionPointCloud& pc,const GeometricPrimitive3D& g)
 {
-  Real dmax = Inf;
+  GeometricPrimitive3D glocal = g;
+  RigidTransform Tinv;
+  Tinv.setInverse(pc.currentTransform);
+  glocal.Transform(Tinv);
+
+  /*
+  AABB3D gbb = glocal.GetAABB();
+  gDistanceTestValue = Inf;
+  gDistanceTestObject = &glocal;
+  pc.grid.BoxQuery(Vector(3,gbb.bmin),Vector(3,gbb.bmax),distanceTest);
+  return gDistanceTestValue;
+  */
   //test all points, linearly
+  Real dmax = Inf;
   for(size_t i=0;i<pc.points.size();i++)
-    dmax = Min(dmax,g.Distance(pc.points[i]));
+    dmax = Min(dmax,glocal.Distance(pc.points[i]));
   return dmax;
+}
+
+static Real gNearbyTestThreshold = 0;
+static GeometricPrimitive3D* gNearbyTestObject = NULL;
+static std::vector<Point3D*> gNearbyTestResults;
+static size_t gNearbyTestBranch = 0;
+bool nearbyTest(void* obj)
+{
+  Point3D* p = reinterpret_cast<Point3D*>(obj);
+  if(gNearbyTestObject->Distance(*p) <= gNearbyTestThreshold)
+    gNearbyTestResults.push_back(p);
+  if(gNearbyTestResults.size() >= gNearbyTestBranch) return false;
+  return true;
 }
 
 void NearbyPoints(const CollisionPointCloud& pc,const GeometricPrimitive3D& g,Real tol,std::vector<int>& points,size_t maxContacts)
@@ -55,12 +151,37 @@ void NearbyPoints(const CollisionPointCloud& pc,const GeometricPrimitive3D& g,Re
   //quick reject test
   if(g.Distance(bb) > tol) return;
 
-  //test all points, linearly
-  for(size_t i=0;i<pc.points.size();i++)
-    if(g.Distance(pc.points[i]) <= tol) {
-      points.push_back(int(i));
-      if(points.size()>=maxContacts) return;
-    }
+  GeometricPrimitive3D glocal = g;
+  RigidTransform Tinv;
+  Tinv.setInverse(pc.currentTransform);
+  glocal.Transform(Tinv);
+
+  AABB3D gbb = glocal.GetAABB();
+  gbb.setIntersection(pc.bblocal);
+  GridSubdivision::Index imin,imax;
+  pc.grid.PointToIndex(Vector(3,gbb.bmin),imin);
+  pc.grid.PointToIndex(Vector(3,gbb.bmax),imax);
+  int numCells = (imax[0]-imin[0]+1)*(imax[1]-imin[1]+1)*(imax[2]-imin[2]+1);
+  if(numCells > (int)pc.points.size()) {
+    printf("Testing all points\n");
+    //test all points, linearly
+    for(size_t i=0;i<pc.points.size();i++)
+      if(glocal.Distance(pc.points[i]) <= tol) {
+	points.push_back(int(i));
+	if(points.size()>=maxContacts) return;
+      }
+  }
+  else {
+    printf("Testing points in BoxQuery\n");
+    gNearbyTestThreshold = tol;
+    gNearbyTestResults.resize(0);
+    gNearbyTestObject = &glocal;
+    gNearbyTestBranch = maxContacts;
+    pc.grid.BoxQuery(Vector(3,gbb.bmin),Vector(3,gbb.bmax),nearbyTest);
+    points.resize(gNearbyTestResults.size());
+    for(size_t i=0;i<points.size();i++)
+      points[i] = gNearbyTestResults[i] - &pc.points[0];
+  }
 }
 
 } //namespace Geometry
--- geometry/CollisionPointCloud.h
+++ geometry/CollisionPointCloud.h
@@ -4,11 +4,14 @@
 #include <meshing/PointCloud.h>
 #include <math3d/geometry3d.h>
 #include <limits.h>
+#include "GridSubdivision.h"
 
 namespace Geometry {
 
   using namespace Math3D;
 
+/** @brief A point cloud with a fast collision detection data structure
+ */
 class CollisionPointCloud : public Meshing::PointCloud3D
 {
  public:
@@ -18,14 +21,26 @@ class CollisionPointCloud : public Meshing::PointCloud3D
   ///way than the constructor
   void InitCollisions();
 
-  //TODO: collision accelerators
+  ///The local bounding box of the point cloud
   AABB3D bblocal;
+  ///The transformation of the point cloud in space 
   RigidTransform currentTransform;
+  Real gridResolution; ///< default value is 0, which auto-determines from point cloud
+  GridSubdivision grid;
 };
 
+///Returns the orientd bounding box of the point cloud
 void GetBB(const CollisionPointCloud& pc,Box3D& b);
+
+///Computes whether any point in the pc is within tol distance of the
+///primitive g. O(min(n,c)) running time, where c is the number of grid
+///cells within distance tol of the bounding box of g.
 bool WithinDistance(const CollisionPointCloud& pc,const GeometricPrimitive3D& g,Real tol);
+///Returns the nearest distance from any point in pc to g.  O(n) running time.
 Real Distance(const CollisionPointCloud& pc,const GeometricPrimitive3D& g);
+///Computes the set of points in the pc that are within tol distance of the
+///primitive g.  O(min(n,c)) running time, where c is the number of grid
+///cells within distance tol of the bounding box of g.
 void NearbyPoints(const CollisionPointCloud& pc,const GeometricPrimitive3D& g,Real tol,std::vector<int>& points,size_t maxContacts=INT_MAX);
 
 } //namespace Geometry
