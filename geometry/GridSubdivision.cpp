687dd669908dd255865c3c4e700bbe307453fafd
krishauser <hauser.kris@gmail.com>
2015-03-15 01:59:18 -0400

    More functionality of grid structures

--- geometry/GridSubdivision.cpp
+++ geometry/GridSubdivision.cpp
@@ -25,6 +25,249 @@ size_t IndexHash::operator () (const std::vector<int>& x) const
 
 
 
+GridHash::GridHash(int numDims,Real _h)
+  :h(numDims,_h)
+{}
+
+GridHash::GridHash(const Vector& _h)
+  :h(_h)
+{}
+
+void GridHash::Set(const Index& i,void* data)
+{
+  assert((int)i.size()==h.n);
+  buckets[i] = data;
+}
+
+void* GridHash::Erase(const Index& i)
+{
+  assert((int)i.size()==h.n);
+  HashTable::iterator bucket = buckets.find(i);
+  if(bucket != buckets.end()) {
+    void* v=bucket->second;
+    buckets.erase(bucket);
+    return v;
+  }
+  return NULL;;
+}
+
+void* GridHash::Get(const Index& i)
+{
+  HashTable::iterator bucket = buckets.find(i);
+  if(bucket != buckets.end()) {
+    return bucket->second;
+  }
+  return NULL;
+}
+
+void GridHash::Clear()
+{
+  buckets.clear();
+}
+
+void GridHash::Enumerate(std::vector<Value>& items) const
+{
+  items.resize(0);
+  for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++)
+    items.push_back(i->second);
+}
+
+void GridHash::PointToIndex(const Vector& p,Index& i) const
+{
+  assert(p.n == h.n);
+  i.resize(p.n);
+  for(int k=0;k<p.n;k++) {
+    assert(h(k) > 0);
+    i[k] = (int)Floor(p(k)/h(k));
+  }
+}
+
+void GridHash::PointToIndex(const Vector& p,Index& i,Vector& u) const
+{
+  assert(p.n == h.n);
+  i.resize(p.n);
+  u.resize(p.n);
+  for(int k=0;k<p.n;k++) {
+    assert(h(k) > 0);
+    u(k) = p(k)-Floor(p(k)/h(k));
+    i[k] = (int)Floor(p(k)/h(k));
+  }
+}
+
+void GridHash::IndexBucketBounds(const Index& i,Vector& bmin,Vector& bmax) const
+{
+  assert((int)i.size() == h.n);
+  bmin.resize(h.n);
+  bmax.resize(h.n);
+  for(int k=0;k<h.n;k++) {
+    bmin(k) = h(k)*(Real)i[k];
+    bmax(k) = bmin(k) + h(k);
+  }
+}
+
+void GridHash::GetRange(Index& imin,Index& imax) const
+{
+  if(buckets.empty()) {
+    imin.resize(h.n);
+    imax.resize(h.n);
+    fill(imin.begin(),imin.end(),0);
+    fill(imax.begin(),imax.end(),0);
+    return;
+  }
+  imin=imax=buckets.begin()->first;
+  for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++) {
+    const Index& idx=i->first;
+    assert((int)idx.size() == h.n);
+    for(size_t k=0;k<idx.size();i++) {
+      if(idx[k] < imin[k]) imin[k] = idx[k];
+      else if(idx[k] > imax[k]) imax[k] = idx[k];
+    }
+  }
+}
+
+void GridHash::GetRange(Vector& bmin,Vector& bmax) const
+{
+  if(buckets.empty()) {
+    bmin.resize(h.n,0);
+    bmax.resize(h.n,0);
+    return;
+  }
+  Index imin,imax;
+  GetRange(imin,imax);
+  bmin.resize(h.n);
+  bmax.resize(h.n);
+  for(int k=0;k<h.n;k++) {
+    bmin(k) = h(k)*(Real)imin[k];
+    bmax(k) = h(k)*(Real)(imax[k]+1);
+  }
+}
+
+bool GridHash::IndexQuery(const Index& imin,const Index& imax,QueryCallback f) const
+{
+  assert(h.n == (int)imin.size());
+  assert(h.n == (int)imax.size());
+  for(size_t k=0;k<imin.size();k++)
+    assert(imin[k] <= imax[k]);
+
+  //check to see if we should just loop through all buckets
+  int numBuckets = 1;
+  for(size_t k=0;k<imin.size();k++)
+    numBuckets *= (imax[k]-imin[k]+1);
+  if(numBuckets >= (int)buckets.size()) {
+    for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++) {
+      bool test = true;
+      for(size_t k=0;k<imin.size();k++)
+	if(i->first[k] < imin[k] || i->first[k] > imax[k]) {
+	  test=false;
+	  break;
+	}
+      if(test) {
+	if(!f(i->second)) return false;
+      }
+    }
+  }
+  else {
+    Index i=imin;
+    for(;;) {
+      HashTable::const_iterator item = buckets.find(i);
+      if(item != buckets.end())
+	if(!f(item->second)) return false;
+      if(IncrementIndex(i,imin,imax)!=0) break;
+    }
+    return true;
+  }
+}
+
+bool GridHash::BoxQuery(const Vector& bmin,const Vector& bmax,QueryCallback f) const
+{
+  Index imin,imax;
+  PointToIndex(bmin,imin);
+  PointToIndex(bmax,imax);
+  return IndexQuery(imin,imax,f);
+}
+
+bool GridHash::BallQuery(const Vector& c,Real r,QueryCallback f) const
+{
+  //TODO: crop out boxes not intersected by sphere?
+  Index imin,imax;
+  Vector u;
+  PointToIndex(c,imin,u);
+  for(int k=0;k<c.n;k++) {
+    int ik=imin[k];
+    Real r_over_h = r/h(k);
+    imin[k] = ik - (int)Floor(u(k)-r_over_h);
+    imax[k] = ik + (int)Floor(u(k)+r_over_h);
+  }
+  return IndexQuery(imin,imax,f);
+}
+
+//TODO: do this with a DDA algorithm
+//bool GridHash::SegmentQuery(const Vector& a,const Vector& b,QueryCallback f) const;
+
+
+
+void GridHash::IndexItems(const Index& imin,const Index& imax,vector<void*>& objs) const
+{
+  assert(h.n == (int)imin.size());
+  assert(h.n == (int)imax.size());
+  for(size_t k=0;k<imin.size();k++)
+    assert(imin[k] <= imax[k]);
+
+  objs.clear();
+  //check to see if we should just loop through all buckets
+  int numBuckets = 1;
+  for(size_t k=0;k<imin.size();k++)
+    numBuckets *= (imax[k]-imin[k]+1);
+  if(numBuckets >= (int)buckets.size()) {
+    for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++) {
+      bool test = true;
+      for(size_t k=0;k<imin.size();k++)
+	if(i->first[k] < imin[k] || i->first[k] > imax[k]) {
+	  test=false;
+	  break;
+	}
+      if(test) {
+	objs.push_back(i->second);
+      }
+    }
+  }
+  else {
+    Index i=imin;
+    for(;;) {
+      HashTable::const_iterator item=buckets.find(i);
+      if(item != buckets.end())
+	objs.push_back(item->second);
+      if(IncrementIndex(i,imin,imax)!=0) break;
+    }
+  }
+}
+
+void GridHash::BoxItems(const Vector& bmin,const Vector& bmax,vector<void*>& objs) const
+{
+  Index imin,imax;
+  PointToIndex(bmin,imin);
+  PointToIndex(bmax,imax);
+  IndexItems(imin,imax,objs);
+}
+
+void GridHash::BallItems(const Vector& c,Real r,vector<void*>& objs) const
+{
+  //TODO: crop out boxes not intersected by sphere?
+  Index imin,imax;
+  Vector u;
+  PointToIndex(c,imin,u);
+  for(int k=0;k<c.n;k++) {
+    int ik=imin[k];
+    Real r_over_h = r/h(k);
+    imin[k] = ik - (int)Floor(u(k)-r_over_h);
+    imax[k] = ik + (int)Floor(u(k)+r_over_h);
+  }
+  IndexItems(imin,imax,objs);
+}
+
+
+
+
 
 bool EraseObject(ObjectSet& b,void* data)
 {
@@ -75,6 +318,15 @@ bool GridSubdivision::Erase(const Index& i,void* data)
 GridSubdivision::ObjectSet* GridSubdivision::GetObjectSet(const Index& i)
 {
   HashTable::iterator bucket = buckets.find(i);
+  if(bucket != buckets.end()) {
+    return &bucket->second;
+  }
+  return NULL;
+}
+
+const GridSubdivision::ObjectSet* GridSubdivision::GetObjectSet(const Index& i) const
+{
+  HashTable::const_iterator bucket = buckets.find(i);
   if(bucket != buckets.end()) {
     return &bucket->second;
   }
@@ -132,7 +384,7 @@ void GridSubdivision::GetRange(Index& imin,Index& imax) const
   for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++) {
     const Index& idx=i->first;
     assert((int)idx.size() == h.n);
-    for(size_t k=0;k<idx.size();i++) {
+    for(size_t k=0;k<idx.size();k++) {
       if(idx[k] < imin[k]) imin[k] = idx[k];
       else if(idx[k] > imax[k]) imax[k] = idx[k];
     }
@@ -163,12 +415,33 @@ bool GridSubdivision::IndexQuery(const Index& imin,const Index& imax,QueryCallba
   for(size_t k=0;k<imin.size();k++)
     assert(imin[k] <= imax[k]);
 
-  Index i=imin;
-  for(;;) {
-    HashTable::const_iterator item = buckets.find(i);
-    if(item != buckets.end())
-      if(!QueryObjects(item->second,f)) return false;
-    if(IncrementIndex(i,imin,imax)!=0) break;
+  //check to see if we should just loop through all buckets
+  int numBuckets = 1;
+  for(size_t k=0;k<imin.size();k++)
+    numBuckets *= (imax[k]-imin[k]+1);
+  if(numBuckets >= (int)buckets.size()) {
+    printf("GridSubdivision: All-bucket iterating\n");
+    for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++) {
+      bool test = true;
+      for(size_t k=0;k<imin.size();k++)
+	if(i->first[k] < imin[k] || i->first[k] > imax[k]) {
+	  test=false;
+	  break;
+	}
+      if(test) {
+	if(!QueryObjects(i->second,f)) return false;
+      }
+    }
+  }
+  else {
+    printf("GridSubdivision: Range iterating\n");
+    Index i=imin;
+    for(;;) {
+      HashTable::const_iterator item = buckets.find(i);
+      if(item != buckets.end())
+	if(!QueryObjects(item->second,f)) return false;
+      if(IncrementIndex(i,imin,imax)!=0) break;
+    }
   }
   return true;
 }
@@ -209,12 +482,31 @@ void GridSubdivision::IndexItems(const Index& imin,const Index& imax,ObjectSet& 
     assert(imin[k] <= imax[k]);
 
   objs.clear();
-  Index i=imin;
-  for(;;) {
-    HashTable::const_iterator item=buckets.find(i);
-    if(item != buckets.end())
-      objs.insert(objs.end(),item->second.begin(),item->second.end());
-    if(IncrementIndex(i,imin,imax)!=0) break;
+  //check to see if we should just loop through all buckets
+  int numBuckets = 1;
+  for(size_t k=0;k<imin.size();k++)
+    numBuckets *= (imax[k]-imin[k]+1);
+  if(numBuckets >= (int)buckets.size()) {
+    for(HashTable::const_iterator i=buckets.begin();i!=buckets.end();i++) {
+      bool test = true;
+      for(size_t k=0;k<imin.size();k++)
+	if(i->first[k] < imin[k] || i->first[k] > imax[k]) {
+	  test=false;
+	  break;
+	}
+      if(test) {
+	objs.insert(objs.end(),i->second.begin(),i->second.end());
+      }
+    }
+  }
+  else {
+    Index i=imin;
+    for(;;) {
+      HashTable::const_iterator item=buckets.find(i);
+      if(item != buckets.end())
+	objs.insert(objs.end(),item->second.begin(),item->second.end());
+      if(IncrementIndex(i,imin,imax)!=0) break;
+    }
   }
 }
 
--- geometry/GridSubdivision.h
+++ geometry/GridSubdivision.h
@@ -23,6 +23,65 @@ struct IndexHash
 };
 
 /** @ingroup Geometry
+ * @brief A grid containing objects (referenced by void pointers)
+ */
+class GridHash
+{
+public:
+  typedef IntTuple Index;
+  typedef void* Value;
+  //called once per value in the query range, return false to stop enumerating
+  typedef bool (*QueryCallback)(void* value);
+
+  GridHash(int numDims,Real h=1);
+  GridHash(const Vector& h);
+  size_t GetBucketCount() const {  return buckets.bucket_count(); }
+  void SetBucketCount(size_t n) {  buckets.rehash(n); }
+  void Set(const Index& i,void* data);
+  ///Important: this method just removes the item from the hash, but does not delete it
+  void* Erase(const Index& i); 
+  void* Get(const Index& i);
+  bool Contains(const Index& i);
+  void Clear();
+  void Enumerate(std::vector<Value>& items) const;
+
+  //returns the index of the point
+  void PointToIndex(const Vector& p,Index& i) const;
+  //same, but with the local coordinates in the bucket [0,1]^n
+  void PointToIndex(const Vector& p,Index& i,Vector& u) const;
+  //returns the lower/upper corner of the bucket
+  void IndexBucketBounds(const Index& i,Vector& bmin,Vector& bmax) const;
+
+  //returns the min/max indices of all occupied cells
+  void GetRange(Index& imin,Index& imax) const;
+  //returns the min/max bound of all occupied cells
+  void GetRange(Vector& bmin,Vector& bmax) const;
+
+  //range imin to imax
+  bool IndexQuery(const Index& imin,const Index& imax,QueryCallback f) const;
+  //bounding box from bmin to bmax
+  bool BoxQuery(const Vector& bmin,const Vector& bmax,QueryCallback f) const;
+  //ball with center c, radius r
+  bool BallQuery(const Vector& c,Real r,QueryCallback f) const;
+  //segment from a to b
+  bool SegmentQuery(const Vector& a,const Vector& b,QueryCallback f) const;
+
+  //range imin to imax
+  void IndexItems(const Index& imin,const Index& imax,std::vector<Value>& items) const;
+  //bounding box from bmin to bmax
+  void BoxItems(const Vector& bmin,const Vector& bmax,std::vector<Value>& items) const;
+  //ball with center c, radius r
+  void BallItems(const Vector& c,Real r,std::vector<Value>& items) const;
+  //segment from a to b
+  void SegmentItems(const Vector& a,const Vector& b,std::vector<Value>& items) const;
+
+  Vector h;
+  typedef std::tr1::unordered_map<Index,Value,IndexHash> HashTable;
+  HashTable buckets;
+};
+
+
+/** @ingroup Geometry
  * @brief A grid with a list of arbitrary objects (given by void pointers)
  */
 class GridSubdivision
@@ -42,6 +101,7 @@ public:
   void Insert(const Index& i,void* data);
   bool Erase(const Index& i,void* data);
   ObjectSet* GetObjectSet(const Index& i);
+  const ObjectSet* GetObjectSet(const Index& i) const;
   void Clear();
 
   //returns the index of the point
