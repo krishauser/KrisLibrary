16978eb91ae243006d9b039f114e48bc3d2e60b8
krishauser <hauser.kris@gmail.com>
2015-06-02 22:17:56 -0400

    Fixed initial estimate of online OLS, added new stochastic least squares method

--- statistics/OnlineOLS.cpp
+++ statistics/OnlineOLS.cpp
@@ -1,5 +1,7 @@
 #include "OnlineOLS.h"
+#include "OnlineLASSO.h"
 #include "DataSet.h"
+#include <spline/Polynomial.h>
 #include <errors.h>
 using namespace Statistics;
 
@@ -33,13 +35,13 @@ void OnlineLeastSquares::SetPrior(const Vector& priorcoeffs,const Vector& streng
 
 bool OnlineLeastSquares::AddPoint(const Vector& _data,Real _outcome)
 {
-  if(numObservations != 0) {
-    Assert(ldl.LDL.m == _data.n);
+  if(coeffs.n != 0) {
+    Assert(coeffs.n == _data.n);
   }
   if(!data.empty()) {
     Assert(_data.n == data[0].n);
   }
-  if(store) {
+  if(store || (int)data.size() < _data.n) {
     data.push_back(_data);
     outcome.push_back(_outcome);
   }
@@ -47,14 +49,23 @@ bool OnlineLeastSquares::AddPoint(const Vector& _data,Real _outcome)
 
   //update the LDL decomposition
   if(coeffs.n == 0) {
-    Assert(store != false);
+    //Assert(store != false);
     //first time
     if(Aty.n == 0) Aty.resize(_data.n,Zero);
     Aty.madd(_data,_outcome);
     if((int)data.size() >= _data.n) {  //enough points to be nondegenerate
+      Matrix AtA(data[0].n,data[0].n);
       DataSet d;
+      Vector xi,xj;
       d.SetObservations(data);
-      ldl.set(d.data);
+      for(int i=0;i<data[0].n;i++){
+	d.GetElement(i,xi);
+	for(int j=0;j<data[0].n;j++) {
+	  d.GetElement(j,xj);
+	  AtA(i,j) = xi.dot(xj);
+	}
+      }
+      ldl.set(AtA);
 
       //estimate coeffs
       ldl.backSub(Aty,coeffs);
@@ -66,7 +77,8 @@ bool OnlineLeastSquares::AddPoint(const Vector& _data,Real _outcome)
     Vector d;
     ldl.getD(d);
     for(int i=0;i<d.n;i++)
-      if(Abs(d(i)) < ldl.zeroTolerance) return false;
+      if(Abs(d(i)) < ldl.zeroTolerance) 
+	return false;
 
     //could also compute Aty from existing coeffs
     Aty.madd(_data,_outcome);
@@ -74,5 +86,83 @@ bool OnlineLeastSquares::AddPoint(const Vector& _data,Real _outcome)
     ldl.backSub(Aty,coeffs);
   }
   return true;
+}
+
+StochasticLeastSquares::StochasticLeastSquares()
+  :numObservations(0),store(false)
+{
+  weightPolynomial.resize(2);
+  weightPolynomial[0] = 1;
+  weightPolynomial[1] = 0.1;
+}
+
+void StochasticLeastSquares::SetPrior(const Vector& _coeffs,int strength)
+{
+  coeffs = _coeffs;
+  numObservations = strength;
+}
+
+void StochasticLeastSquares::AddPoint(const Vector& data,Real outcome)
+{
+  if(numObservations == 0) coeffs = data*outcome;
+  numObservations += 1;
+  Spline::Polynomial<double> P;
+  P.coef = weightPolynomial;
+  Real w = 1.0/P.Evaluate(numObservations);
+  Real err = data.dot(coeffs) - outcome;
+  Real l2 = data.dot(data);
+  if(Abs(l2) > Epsilon) {
+    coeffs.madd(data,-err*w/l2);
+  }
+  if(store) {
+    this->data.push_back(data);
+    this->outcome.push_back(outcome);
+  }
+}
+
+
+StochasticPseudoLASSO::StochasticPseudoLASSO(Real _alpha)
+  :alpha(_alpha),numObservations(0)
+{
+  weightPolynomial.resize(2);
+  weightPolynomial[0] = 1;
+  weightPolynomial[1] = 0.1;
+}
+
+void StochasticPseudoLASSO::SetPrior(const Vector& _coeffs,int strength)
+{
+  coeffs = _coeffs;
+  numObservations = strength;
+}
+
+void StochasticPseudoLASSO::AddPoint(const Vector& data,Real outcome)
+{
+  if(numObservations==0) 
+    coeffs.resize(data.size(),0.0);
+  numObservations += 1;
+  Real err = outcome - data.dot(coeffs);
+  Real maxd = 0;
+  int maxdindex = -1;
+  for(int i=0;i<data.n;i++) {
+    if(Abs(data(i)*err) > 0.5*alpha) {
+      //candidate
+      if(Abs(data(i)) > maxd) {
+	maxd = Abs(data(i));
+	maxdindex = i;
+      }
+    }
+  }
+  if(maxdindex < 0 || maxd < Epsilon) {
+    //no need to change
+    return;
+  }
+  Real xi = data(maxdindex);
+  Real newthetai = (err*xi - alpha*0.5*Sign(err*xi))/Sqr(xi);
+
+  Spline::Polynomial<double> P;
+  P.coef = weightPolynomial;
+  Real w = 1.0/P.Evaluate(numObservations);
+
+  coeffs(maxdindex) += w * newthetai;
 }
 
--- statistics/OnlineOLS.h
+++ statistics/OnlineOLS.h
@@ -28,6 +28,44 @@ struct OnlineLeastSquares
   Vector coeffs;  //stores estimate of coeffs
 };
 
+/** @brief Stochastic gradient descent estimation of least-squares solution
+ * to y = A x
+ *
+ * The A matrix is the "data" vector, y is the "outcome" vector.
+ * Each update step takes O(n) time, where n is the number of
+ * dimensions in x.
+ *
+ * Single step error-minimizing least-squares solution gives
+ *   min ||x-coeffs||^2 s.t. am^T x = bm
+ * => 2(x-coeffs) + lambda * am = 0, am^T x = bm
+ *   x = coeffs + lambda*am
+ * am^T x = bm = am^T coeffs + lambda* am^T am
+ * lambda = (bm - am^T coeffs) / am^T am
+ * => x - coeffs = am * (bm - am^T coeffs) / am^T am
+ * Uses the stochastic gradient descent step
+ *   coeffs(m) = coeffs(m-1) + w(m)*(x - coeffs).
+ * where w(m) is a weight term.
+ * 
+ * The weight is 1 / P(m) where m is the number of observations, and P(m)
+ * is a polynomial.  If P(m) is constant, then the estimate "forgets" distant
+ * history.  If P(m) is linear, then it converges to the true estimate.
+ */
+struct StochasticLeastSquares
+{
+  StochasticLeastSquares();
+  void SetPrior(const Vector& coeffs,int strength=1);
+  void AddPoint(const Vector& data,Real outcome);
+
+  int numObservations;
+  bool store;
+  std::vector<Vector> data;
+  std::vector<Real> outcome;
+
+  std::vector<Real> weightPolynomial;
+  Vector coeffs;  //stores estimate of coeffs
+};
+
+
 } //namespace Statistics
 
 #endif
