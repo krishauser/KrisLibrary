91ef4019dfd0c955d1785ab3584a9988541753a6
krishauser <hauser.kris@gmail.com>
2015-11-02 20:09:49 -0500

    Added some documentation

--- camera/viewport.h
+++ camera/viewport.h
@@ -9,30 +9,36 @@ class Viewport : public Camera
 {
 public:
 	Viewport();
-	void setPerspective(bool);
+	///Sets the viewport as a perspective or orthographic viewport
+	void setPerspective(bool enabled);
+	///Sets the absolute lens angle in the width direction
 	void setLensAngle(float rads);
 
+	///Returns true if the point (mx,my) is within the viewport
 	bool clicked(int mx, int my) const;
+	///Multiplies the camera's zoom by the factor s
 	void zoom(float s);
+	///Scrolls the viewport by the vector x,y in a screen-parallel
+	///local camera frame.
 	void scroll(float x, float y, float z=0);
 
-	//returns the forward direction 
+	///Returns the forward direction in world coordinates 
 	void getViewVector(Vector3&) const;
-	//returns a screen-parallel world-space vector for a screen-space
-	//displacement of dx,dy
+	///Returns a screen-parallel world-space vector for a screen-space
+	///displacement of dx,dy
 	void getMovementVector(float dx, float dy, Vector3&) const;
-	//returns a screen-parallel world-space vector that displaces a point
-	//at distance dist by dx in the screen's x direction and dy in the y
-	//direction
+	///Returns a screen-parallel world-space vector that displaces a point
+	///at distance dist by dx in the screen's x direction and dy in the y
+	///direction
 	void getMovementVectorAtDistance(float dx, float dy, float dist, Vector3&) const;
 	
-	//find the point/direction on the image plane corresponding to a screen
-	//space point
+	///find the point/direction on the image plane corresponding to a
+	///screen space point
 	void getClickVector(float mx, float my, Vector3&) const;
 	void getClickSource(float mx, float my, Vector3&) const;
 
-	//Computes the screen coordinates of a point. Returns true if the point
-	//is within the view frustum.
+	///Computes the screen coordinates of a point pt in world space.
+	///Returns true if the point is within the view frustum.
 	bool project(const Vector3& pt,float& mx,float& my,float& mz) const;
 
 	bool perspective;
