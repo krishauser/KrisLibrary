60768efb7316325f663eab78c9f91438b7de11a3
motion <motion@scovel.egr.duke.edu>
2016-05-08 12:45:35 -0400

    Moved defined function to a cpp file

--- graph/IO.h
+++ graph/IO.h
@@ -11,72 +11,19 @@
 
 namespace Graph {
 
-bool Load_TGF(std::istream& in,Graph<std::string,std::string>& G)
-{
-  G.Cleanup();
-  int numNodes=0;
-  int line=0;
-  std::map<int,int> idsToIndices;
-  std::string s;
-  while(in) {
-    line++;
-    getline(in,s);
-    if(!in) break;
+/** @ingroup Graph
+ * @brief Loads a graph from the Trivial Graph Format.
+ */
+bool Load_TGF(std::istream& in,Graph<std::string,std::string>& G);
 
-    std::stringstream ss(s);
-    ss>>s;
-    if(!ss) continue;  //nothing on line
-    if(s == "#") //now read the edges
-      break;
-    if(!IsValidInteger(s.c_str()))
-      return false;
-    int id=atoi(s.c_str());
-    idsToIndices[id] = numNodes;
-    numNodes++;
-    G.Resize(numNodes);
-    //here's where you might load node data from ss
-    std::getline(ss,G.nodes.back());
-  }
-  if(!in) return false;
+/** @ingroup Graph
+ * @brief Saves a graph to the Trivial Graph Format.
+ */
+void Save_TGF(std::ostream& o,const Graph<std::string,std::string>& G);
 
-  //load edges
-  while(in) {
-    line++;
-    getline(in,s);
-    if(!in) break;
-
-    std::stringstream ss(s);
-    int id1,id2;
-    ss>>id1>>id2;
-    if(ss.bad()) return false;
-    if(idsToIndices.count(id1)==0 || idsToIndices.count(id2)==0)
-      return false;
-    
-    //here's where you might load edge data from ss
-    std::getline(ss,s);
-    G.AddEdge(idsToIndices[id1],idsToIndices[id2],s);
-  }
-  if(in.bad()) return false;
-  return true;
-}
-
-void Save_TGF(std::ostream& o,const Graph<std::string,std::string>& G)
-{
-  for(size_t i=0;i<G.nodes.size();i++) {
-    o<<i+1<<" "<<G.nodes[i]<<std::endl;
-  }
-  o<<"#"<<std::endl;
-  for(size_t i=0;i<G.nodes.size();i++) {
-    EdgeIterator<std::string> e;
-    G.Begin(i,e);
-    while(!e.end()) {
-      o<<e.source()+1<<" "<<e.target()+1<<" "<<*e<<std::endl;
-      e++;
-    }
-  }
-}
-
-
+/** @ingroup Graph
+ * @brief Serializes the graph's nodes using the ostream << operator.
+ */
 template <class N,class E>
 void NodesToStrings(const Graph<N,E>& G,Graph<std::string,std::string>& Gs)
 {
@@ -88,6 +35,9 @@ void NodesToStrings(const Graph<N,E>& G,Graph<std::string,std::string>& Gs)
   }
 }
 
+/** @ingroup Graph
+ * @brief De-serializes the graph's nodes using the istream >> operator.
+ */
 template <class N,class E>
 bool NodesFromStrings(const Graph<std::string,std::string>& Gs,Graph<N,E>& G)
 {
@@ -100,7 +50,9 @@ bool NodesFromStrings(const Graph<std::string,std::string>& Gs,Graph<N,E>& G)
   return true;
 }
 
-
+/** @ingroup Graph
+ * @brief Serializes the graph's nodes and edges using the ostream << operator.
+ */
 template <class N,class E>
 void NodesEdgesToStrings(const Graph<N,E>& G,Graph<std::string,std::string>& Gs)
 {
@@ -125,6 +77,9 @@ void NodesEdgesToStrings(const Graph<N,E>& G,Graph<std::string,std::string>& Gs)
   }
 }
 
+/** @ingroup Graph
+ * @brief De-serializes the graph's nodes and edges using the istream >> operator.
+ */
 template <class N,class E>
 bool NodesEdgesFromStrings(const Graph<std::string,std::string>& Gs,Graph<N,E>& G)
 {
