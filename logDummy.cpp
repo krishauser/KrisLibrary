64c62e7ffea500f0e7289f62e7d9a445470090dc
Kris Hauser <kris.hauser@duke.edu>
2017-05-05 17:21:22 -0400

    changed logStandin

--- logDummy.cpp
+++ logDummy.cpp
@@ -1,4 +1,5 @@
 #pragma once
 #include <log4cxx/logger.h>
 
-static log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("KrisLibrary"));
+//static log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("KrisLibrary"));
+static log4cxx::LoggerPtr logger(log4cxx::Logger::getRootLogger());
