1ff2c52c4cf1e5c54aa5320f1f141d5671647a99
Kris Hauser <kris.hauser@duke.edu>
2018-05-03 18:03:39 -0400

    Updated for C++11 build

--- math/infnan.cpp
+++ math/infnan.cpp
@@ -11,7 +11,7 @@ int IsNaN(double x)
 #ifdef _MSC_VER
   return _isnan(x);
 #elif HAVE_DECL_ISNAN
-  return isnan(x);
+  return std::isnan(x);
 #elif HAVE_IEEE_COMPARISONS
   return (x!=x?1:0);
 #else
@@ -45,7 +45,7 @@ int IsInf(double x)
   else if(cls == _FPCLASS_NINF) return -1;
   else return 0;
 #elif HAVE_DECL_ISINF
-  if(isinf(x)) {
+  if(std::isinf(x)) {
     if(x > 0) return 1;
     else return -1;
   }
@@ -69,7 +69,7 @@ int IsNaN(float x)
   return _isnan(x);
 #elif HAVE_DECL_ISNAN
   //return isnanf(x);
-  return isnan(x);
+  return std::isnan(x);
 #elif HAVE_IEEE_COMPARISONS
   return (x!=x?1:0);
 #else
@@ -104,7 +104,7 @@ int IsInf(float x)
   else return 0;
 #elif HAVE_DECL_ISINF
   //if(isinff(x)) {
-  if(isinf(x)) {
+  if(std::isinf(x)) {
     if(x > 0) return 1;
     else return -1;
   }
