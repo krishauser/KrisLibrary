1455e3d83e860da8eb0e716cd84b6ec7fcf0fdaa
Kris Hauser <kris.hauser@duke.edu>
2018-05-03 21:30:53 -0400

    Changed int comparison to correctly handle pointers in C++11

--- myfile.cpp
+++ myfile.cpp
@@ -189,7 +189,7 @@ void File::Close()
 {
     if(srctype == MODE_MYFILE && impl->file != INVALID_FILE_POINTER) FileClose(impl->file);
 	if(srctype == MODE_MYDATA && impl->datafile != NULL) free(impl->datafile);
-	if((srctype == MODE_TCPSOCKET || srctype==MODE_UDPSOCKET) && impl->file > 0) SocketClose(impl->socket);
+	if((srctype == MODE_TCPSOCKET || srctype==MODE_UDPSOCKET) && impl->file != NULL) SocketClose(impl->socket);
 
 	srctype = MODE_NONE;
 	mode = 0;
