8363484dca9f282d9564d8fce95c7132e63b0dc1
Kris Hauser <kris.hauser@duke.edu>
2018-08-15 16:05:26 -0400

    More documentation

--- Logger.cpp
+++ Logger.cpp
@@ -8,104 +8,113 @@
 #include "Logger.h"
 #include <stdio.h>
 
+namespace KrisLibrary {
+
 //static log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("KrisLibrary"));
 bool rootLoggerDefined = false;
 log4cxx::LoggerPtr rootLogger = 0;
 log4cxx::LoggerPtr myLogger = 0;
 std::string logName = "";
-namespace KrisLibrary{
-	log4cxx::LoggerPtr logger(){
-		if (rootLoggerDefined){
-			return rootLogger;
+
+log4cxx::LoggerPtr logger()
+{
+	if (rootLoggerDefined){
+		return rootLogger;
+	}
+
+	rootLogger = log4cxx::Logger::getRootLogger();
+	try{
+		log4cxx::xml::DOMConfigurator::configure("./log4cxx.xml");
+		if (rootLogger->getAllAppenders().empty()){
+			log4cxx::LayoutPtr defaultLayout   = new log4cxx::PatternLayout("%m %n");
+       		log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
+       		rootLogger->addAppender(defaultAppender);
+       		rootLogger->setLevel(log4cxx::Level::getInfo());
+       		logName = "root";
+			printf("KrisLibrary::logger(): configured as default\n");
+			//log4cxx::BasicConfigurator::configure();
 		}
 
-		rootLogger = log4cxx::Logger::getRootLogger();
+	}
+	catch(...){
+		log4cxx::LayoutPtr defaultLayout   = new log4cxx::PatternLayout("%m %n");
+       	log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
+       	rootLogger->addAppender(defaultAppender);
+       	rootLogger->setLevel(log4cxx::Level::getInfo());
+		printf("KrisLibrary::logger(): configured as default\n");
+		logName = "root";
+		//log4cxx::BasicConfigurator::configure();
+	}
+	
+	rootLoggerDefined = true;
+	return rootLogger;
+}
+
+log4cxx::LoggerPtr logger(const char* s)
+{
+	
+	if(rootLoggerDefined){
+		//we have read the file or at least set a root logger previously
+		try{
+			printf("KrisLibrary::logger(): trying to find logger %s\n", s);
+			myLogger = log4cxx::Logger::getLogger(s);
+			if(myLogger->getAllAppenders().empty()){
+				printf("  Logger %s has no appenders. Returning root logger.\n", s);
+				return rootLogger;
+			}
+		}
+		catch(...){
+			//logger i
+			printf("  Logger %s is not defined. Returning root logger.\n", s);
+			return rootLogger;
+		}
+	}else{
+		// otherwise, logger needs to be configured
 		try{
 			log4cxx::xml::DOMConfigurator::configure("./log4cxx.xml");
-			if (rootLogger->getAllAppenders().empty()){
+			myLogger = log4cxx::Logger::getLogger(s);
+
+			if (myLogger->getAllAppenders().empty()){
+				//sets up basic setup if logger doesn't have appender set up.
 				log4cxx::LayoutPtr defaultLayout   = new log4cxx::PatternLayout("%m %n");
-	       		log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
-	       		rootLogger->addAppender(defaultAppender);
-	       		rootLogger->setLevel(log4cxx::Level::getInfo());
-	       		logName = "root";
+			    log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
+			    myLogger->addAppender(defaultAppender);
+			    myLogger->setLevel(log4cxx::Level::getInfo());
 				printf("KrisLibrary::logger(): configured as default\n");
 				//log4cxx::BasicConfigurator::configure();
 			}
 
+
 		}
 		catch(...){
+			//catches null logger errors
+			myLogger = rootLogger = log4cxx::Logger::getRootLogger();
+			printf("KrisLibrary::logger(): Hit error. Setting up default logger\n");
 			log4cxx::LayoutPtr defaultLayout   = new log4cxx::PatternLayout("%m %n");
-	       	log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
-	       	rootLogger->addAppender(defaultAppender);
-	       	rootLogger->setLevel(log4cxx::Level::getInfo());
-			printf("KrisLibrary::logger(): configured as default\n");
-			logName = "root";
+       		log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
+       		rootLogger->addAppender(defaultAppender);
+       		rootLogger->setLevel(log4cxx::Level::getInfo());  // Log level set to INFO
 			//log4cxx::BasicConfigurator::configure();
+			rootLoggerDefined = true;
 		}
-		
-		rootLoggerDefined = true;
-		return rootLogger;
-	}
-	
-	log4cxx::LoggerPtr logger(const char* s){
-		
-		if(rootLoggerDefined){
-			//we have read the file or at least set a root logger previously
-			try{
-				printf("KrisLibrary::logger(): trying to find logger %s\n", s);
-				myLogger = log4cxx::Logger::getLogger(s);
-				if(myLogger->getAllAppenders().empty()){
-					printf("  Logger %s has no appenders. Returning root logger.\n", s);
-					return rootLogger;
-				}
-			}
-			catch(...){
-				//logger i
-				printf("  Logger %s is not defined. Returning root logger.\n", s);
-				return rootLogger;
-			}
-		}else{
-			// otherwise, logger needs to be configured
-			try{
-				log4cxx::xml::DOMConfigurator::configure("./log4cxx.xml");
-				myLogger = log4cxx::Logger::getLogger(s);
-
-				if (myLogger->getAllAppenders().empty()){
-					//sets up basic setup if logger doesn't have appender set up.
-					log4cxx::LayoutPtr defaultLayout   = new log4cxx::PatternLayout("%m %n");
-				    log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
-				    myLogger->addAppender(defaultAppender);
-				    myLogger->setLevel(log4cxx::Level::getInfo());
-					printf("KrisLibrary::logger(): configured as default\n");
-					//log4cxx::BasicConfigurator::configure();
-				}
-
-
-			}
-			catch(...){
-				//catches null logger errors
-				myLogger = rootLogger = log4cxx::Logger::getRootLogger();
-				printf("KrisLibrary::logger(): Hit error. Setting up default logger\n");
-				log4cxx::LayoutPtr defaultLayout   = new log4cxx::PatternLayout("%m %n");
-	       		log4cxx::AppenderPtr defaultAppender = new log4cxx::ConsoleAppender(defaultLayout);
-	       		rootLogger->addAppender(defaultAppender);
-	       		rootLogger->setLevel(log4cxx::Level::getInfo());  // Log level set to INFO
-				//log4cxx::BasicConfigurator::configure();
-				rootLoggerDefined = true;
-			}
-			return myLogger;
-		}
-	
-		logName = std::string(s);
 		return myLogger;
 	}
 
-	void loggerWait(){
-
-		if(KrisLibrary::logger()->isEnabledFor(log4cxx::Level::getDebug())){
-			getchar();	
-		} 
-	}
-
+	logName = std::string(s);
+	return myLogger;
 }
+
+void loggerWait()
+{
+	loggerWait(KrisLibrary::logger());
+}
+
+void loggerWait(log4cxx::LoggerPtr logger)
+{
+	if(logger->isEnabledFor(log4cxx::Level::getDebug())){
+		getchar();	
+	} 
+}
+
+} //namespace KrisLibrary
 
--- Logger.h
+++ Logger.h
@@ -1,15 +1,37 @@
-
-#ifndef __LOGGER_H_INCLUDED__
-#define __LOGGER_H_INCLUDED__
+#ifndef KRISLIBRARY_LOGGER_H
+#define KRISLIBRARY_LOGGER_H
 
 #include <log4cxx/logger.h>
 
-namespace KrisLibrary{
-	
-	extern log4cxx::LoggerPtr logger();
-	extern log4cxx::LoggerPtr logger(const char*);
-	extern void loggerWait();
-}
+/** @file Logger.h
+ * @brief The logging system used in KrisLibrary.
+ */
+
+
+namespace KrisLibrary {
+
+/** @brief Retrieves the base logger. 
+ *
+ * To log, call LOG4CXX_INFO(KrisLibrary::logger(),msg...), or LOG4CXX_WARN, LOG4CXX_ERROR, etc.
+ */
+extern log4cxx::LoggerPtr logger();
+
+/** @brief Retrieves a named logger. 
+ *
+ * It can be used by calling LOG4CXX_INFO(KrisLibrary::logger(name),msg...), or LOG4CXX_WARN, LOG4CXX_ERROR, etc.
+ * However, this is not the most efficient method, since logger lookup is performed for each message.  Instead
+ * you should save the LoggerPtr and use it multiple times.  The DEFINE_LOGGER and GET_LOGGER macros are
+ * better for this, since they only perform lookup of the logger on the first call.
+ */
+extern log4cxx::LoggerPtr logger(const char* name);
+
+/** @brief If the root logger is enabled for debug level, this will cause a getchar() to be called.
+ */
+extern void loggerWait();
+
+/** @brief If logger is enabled for debug level, this will cause a getchar() to be called.
+ */
+extern void loggerWait(log4cxx:LoggerPtr logger);
 
 ///Use this inside a cpp file to define a fast logger 
 #define DEFINE_LOGGER(name) \
@@ -32,6 +54,8 @@ namespace KrisLibrary{
 ///Use this to retrieve a fast logger
 #define GET_LOGGER(name) KrisLibrary::_get_logger_##name()
 
+} //namespace KrisLibrary
+
   
 
-#endif // __LOGGER_H_INCLUDED__
+#endif // KRISLIBRARY_LOGGER_H
